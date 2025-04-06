e40d09ff068592747425ddc69404f732cf5ddd3b
raviranjan0 <raviranjankrsingh07@gmail.com>
2025-04-06 17:47:24 +0530

    Login Page created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
-const login = () => {
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const {login} = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+   
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        { email, password }
+      );
+     if(response.data.success){
+      login(response.data.user)
+      localStorage.setItem("token", response.data.token)
+      if(response.data.user.role === "admin"){
+        navigate('/admin-dashboard')
+      }else{
+        navigate('/employee-dashboard')
+      }
+     }
+
+    } catch (error) {
+  if (error.response && error.response.data && !error.response.data.success) {
+    setError(error.response.data.error); // Access the 'error' field
+  } else {
+    setError("Server error. Please try again later.");
+  }
+}
+  };
+
   return (
     <div
       className="flex flex-col items-center h-screen justify-center
@@ -10,23 +46,34 @@ const login = () => {
         Employee Management System
       </h2>
       <div className="border shadow p-6 w-80 bg-white">
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        {error && <p className="text-red-500">{error}</p>}
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">Email:</label>
-            <input type="email"
-            className="w-full px-3 py-2 border"
-             placeholder="enter email" 
-             />
+            <label htmlFor="email" className="block text-gray-700">
+              Email:
+            </label>
+            <input
+              type="email"
+              className="w-full px-3 py-2 border"
+              placeholder="enter email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
 
           <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700">Password:</label>
-            <input type="password"
-           className="w-full px-3 py-2 border"
-             placeholder="........" 
-             />
+            <label htmlFor="password" className="block text-gray-700">
+              Password:
+            </label>
+            <input
+              type="password"
+              className="w-full px-3 py-2 border"
+              placeholder="........"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
 
           <div className="mb-4 flex items-center justify-between">
@@ -40,11 +87,12 @@ const login = () => {
           </div>
 
           <div className="mb-4">
-          <button
-          type="submit"
-          className="w-full bg-teal-600 text-white py-2"
-          >
-            Login</button>
+            <button
+              type="submit"
+              className="w-full bg-teal-600 text-white py-2"
+            >
+              Login
+            </button>
           </div>
         </form>
       </div>
@@ -52,4 +100,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default LoginPage;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import authRouter from './routes/auth.js'
+import connectToDatabase from './models/db/db.js';
+
+connectToDatabase();
 
 const Port = process.env.PORT;
 
 const app = express();
 app.use(cors(), express.json());
+app.use('/api/auth', authRouter)
 
 app.listen(Port, ()=>{
     console.log(`Server is running at ${Port}`);
