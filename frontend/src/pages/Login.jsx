a43e916a1114f8232056821474a75e9c1646ef96
raviranjan0 <raviranjankrsingh07@gmail.com>
2025-04-06 17:50:52 +0530

    font family changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,7 +42,7 @@ const LoginPage = () => {
       className="flex flex-col items-center h-screen justify-center
     bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6"
     >
-      <h2 className="font-pacifico text-3xl text-white">
+      <h2  style={{ fontFamily: 'Pacifico, cursive' }} className=" text-4xl text-white">
         Employee Management System
       </h2>
       <div className="border shadow p-6 w-80 bg-white">
